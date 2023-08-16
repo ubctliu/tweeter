95ac16da4eb0694a25c42fdf575e5f4142ed78a4
Terrence <terrence.liu@alumni.ubc.ca>
2023-08-16 14:59:05 -0700

    Implement cross-site scripting & reload tweets on submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,29 +3,61 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(() => {
-  $.get("/tweets")
-    .done(tweets => {
-      for (const tweet of tweets) {
-        const date = new Date(tweet.created_at);
-        const differenceInDays = Math.round((new Date().getTime() - date.getTime()) / (1000 * 3600 * 24));
-        const dayOrDays = differenceInDays > 1 ? "days ago" : "day ago";
-        $(".tweets").prepend(`<article class="tweet">
+const escape = (str) => {
+  return $('<div>').text(str).html();
+};
+
+const createTweetElement = (tweet) => {
+  const date = timeago.format(tweet.created_at);
+  const $tweet = `<article class="tweet">
         <header>
         <img src=${tweet.user.avatars}></img>
           <h3>${tweet.user.name}</h3>
           <span class="user-handle">${tweet.user.handle}</span>
         </header>
-          <p class="tweet-content">${tweet.content.text}</p>
+          <p class="tweet-content">${escape(tweet.content.text)}</p>
         <footer> 
-          <time>${differenceInDays} ${dayOrDays}</time>
+          <time>${date}</time>
           <div class="icons">
           <span class="fa-solid fa-flag"></span>
           <span class="fa-solid fa-retweet"></span>
           <span class="fa-solid fa-heart"></span>
         </div>
         </footer>
-      </article>`);
-      }
+      </article>`;
+  return $tweet;
+};
+
+const renderTweets = (tweets) => {
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $(".tweets").prepend($tweet);
+  }
+};
+
+$(document).ready(() => {
+  $.get("/tweets")
+    .done(tweets => {
+      renderTweets(tweets);
     });
+  $(".new-tweet").on("submit", (event) => {
+    event.preventDefault();
+    const $formData = $(event.target).serialize();
+    const $formDataLength = 140 - $(event.target).find(".counter").text();
+    if ($formDataLength <= 140) {
+      if ($formDataLength > 0) {
+        $.post("/tweets", $formData, (response) => {
+          console.log(response);
+          $.get("/tweets")
+            .done(tweets => {
+              renderTweets(tweets);
+            });
+        });
+      } else {
+        alert("Tweet cannot be empty!");
+      }
+    } else {
+      alert("Tweet cannot exceed 140 characters!");
+    }
+  });
 });
